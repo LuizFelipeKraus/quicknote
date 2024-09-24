cf86e6fdf66bbde78444537439c1abeedca9cc32
Luiz Felipe Kraus <luiz.kraus@ALFA.local>
2024-09-24 14:15:16 -0300

    aula 31

--- cmd/http/main.go
+++ cmd/http/main.go
@@ -19,30 +19,55 @@ func noteView(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(r.URL.Path)
 	fmt.Println(r.URL.RawQuery)
 
+	file := []string{
+		"views/templates/base.html",
+		"views/templates/pages/note-view.html",
+	}
+
 	id := r.URL.Query().Get("id")
 	if id == "" {
 		http.Error(w, "Nota não encontrada!", http.StatusNotFound)
 		return
 	}
 
-	t, err := template.ParseFiles("views/templates/noteView.html")
+	t, err := template.ParseFiles(file...)
 	if err != nil {
+		fmt.Println(err)
 		http.Error(w, "Aconteceu um erro ao executar!", http.StatusInternalServerError)
 		return
 	}
 
-	t.Execute(w, id)
+	t.ExecuteTemplate(w, "base", id)
 }
 
 func noteList(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("views/templates/home.html")
+	file := []string{
+		"views/templates/base.html",
+		"views/templates/pages/home.html",
+	}
+	t, err := template.ParseFiles(file...)
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, "Aconteceu um erro ao executar!", http.StatusInternalServerError)
+		return
+	}
+
+	t.ExecuteTemplate(w, "base", nil)
+
+}
+
+func noteNew(w http.ResponseWriter, r *http.Request) {
+	file := []string{
+		"views/templates/base.html",
+		"views/templates/pages/note-new.html",
+	}
+	t, err := template.ParseFiles(file...)
 	if err != nil {
 		http.Error(w, "Aconteceu um erro ao executar!", http.StatusInternalServerError)
 		return
 	}
 
-	t.Execute(w, nil)
-
+	t.ExecuteTemplate(w, "base", nil)
 }
 
 func main() {
@@ -51,6 +76,7 @@ func main() {
 
 	mux.HandleFunc("/", noteList)
 	mux.HandleFunc("/note/view", noteView)
+	mux.HandleFunc("/note/new", noteNew)
 	mux.HandleFunc("/note/create", noteCreate)
 
 	http.ListenAndServe(":5000", mux)
