4aba8675a6abe8d8d41614f82ff1da6a57e70d3c
Luiz Felipe Kraus <luiz.kraus@ALFA.local>
2024-09-30 16:35:36 -0300

    aula 37

--- cmd/exp/exp.go
+++ cmd/exp/exp.go
@@ -1,20 +1,16 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"os"
-
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		panic(err)
-	}
+	port := flag.String("port", "7000", "Server Port")
+	flag.Parse()
+	fmt.Println("Server is running on port", *port)
 
-	port := os.Getenv("PORT")
-	host := os.Getenv("HOST")
-	fmt.Printf("%s:%s", host, port)
-
+	var port2 string
+	flag.StringVar(&port2, "port2", "6000", "Server Port")
+	fmt.Println("Server is running on port", port2)
 }
